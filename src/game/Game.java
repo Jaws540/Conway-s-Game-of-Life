c46443c0a904309e4cc5415612da0c0b02eed9bc
Knotsman <jaws540@gmail.com>
2016-09-12 16:32:30 -0400

    Update Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,15 +9,21 @@ import java.util.ArrayList;
 public class Game {
 	
 	private static Canvas c;
-	private static boolean isRunning = true;
+	private static boolean isRunning = false;
 	private static double ticksPerSecond = 60.0;
 	private static int cellSize = 20;
 	
-	private ArrayList<Cell> cells = new ArrayList<Cell>();
+	private static ArrayList<Cell> cells = new ArrayList<Cell>();
 	private ArrayList<Cell> changedCells = new ArrayList<Cell>();
 	
 	public Game(Canvas c){
 		Game.c = c;
+		for(int y = 0; y < RES.HEIGHT / getCellSize(); y++){
+			for(int x = 0; x < RES.WIDTH / getCellSize(); x++){
+				cells.add(new Cell(x + (y * (RES.WIDTH / getCellSize())), false));
+				changedCells.add(new Cell(x + (y * (RES.WIDTH / getCellSize())), false));
+			}
+		}
 		Loop l = new Loop();
 		l.start();
 	}
@@ -34,8 +40,17 @@ public class Game {
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, RES.WIDTH, RES.HEIGHT);
 		
-		
-		
+		for(int y = 0; y < RES.HEIGHT; y += getCellSize()){
+			for(int x = 0; x < RES.WIDTH; x += getCellSize()){
+				System.out.println(x + (y * (RES.WIDTH / getCellSize())));
+				if(Game.cells.get(x + (y * (RES.WIDTH / getCellSize()))).isAlive()){
+					g.setColor(Color.BLUE);
+				}else{
+					g.setColor(Color.WHITE);
+				}
+				g.fillRect(x, y, getCellSize(), getCellSize());
+			}
+		}
 		
 		g.setColor(Color.BLACK);
 		for(int y = 0; y < RES.HEIGHT; y += getCellSize()){
@@ -70,12 +85,18 @@ public class Game {
 		Game.ticksPerSecond = ticksPerSecond;
 	}
 
-	public ArrayList<Cell> getCells() {
+	public static ArrayList<Cell> getCells() {
 		return cells;
 	}
 
 	public void setCells(ArrayList<Cell> cells) {
-		this.cells = cells;
+		Game.cells = cells;
+	}
+	
+	public static void setCellState(int index, boolean alive){
+		Cell c = Game.getCells().get(index);
+		c.setAlive(alive);
+		Game.cells.set(index, c);
 	}
 
 	public ArrayList<Cell> getChangedCells() {
